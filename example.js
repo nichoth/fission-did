7790e09a17c9cc6729363b89957e0d5ae2e1039f
nichoth <nichoth@gmail.com>
2021-09-01 23:33:03 -0700

    WIP

--- example.js
+++ example.js
@@ -1,2 +1,31 @@
 import * as wn from "webnative"
 
+console.log('wn', wn)
+
+// DIDs
+// const ourDID = await wn.did.ucan()
+wn.did.ucan()
+    .then(ourDID => {
+        console.log('our did', ourDID)
+
+        /**
+         * The UCAN, encoded as a string.
+         */
+        wn.ucan.build({
+            audience: otherDID,
+            issuer: ourDID,
+            lifetimeInSeconds: 60 * 60 * 24, // UCAN expires in 24 hours
+            proof: possibleProof
+        })
+            .then((ucan) => {
+                console.log('got ucan', ucan)
+            })
+    })
+
+const otherDID = "did:key:EXAMPLE"
+
+/**
+ * This can be another UCAN which has a bigger, or equal,
+ * set of permissions than the UCAN we're building later.
+ */
+const possibleProof = null // or, other UCAN.
---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default {
+    input: 'example.js',
+    output: {
+        file: 'public/bundle.js',
+        format: 'umd',
+        name: 'fissionDID',
+        sourcemap: true
+    }
+}
