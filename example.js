b2e6749a29da33ff5bfd2c2a121065a57a9eafd8
nichoth <nichoth@gmail.com>
2021-09-02 15:43:11 -0700

    more stuff

--- example.js
+++ example.js
@@ -7,27 +7,61 @@ console.log('wn', wn)
 // DIDs
 // const ourDID = await wn.did.ucan()
 wn.did.ucan()
-    .then(ourDID => {
+    .then(async ourDID => {
         console.log('our did', ourDID)
+
+        var pk = wn.did.didToPublicKey(ourDID)
+        console.log('pk', pk)
 
         /**
          * This can be another UCAN which has a bigger, or equal,
-         * set of permissions than the UCAN we're building later.
+         * set of permissions than the UCAN we're building.
          */
         const possibleProof = null // or, other UCAN.
 
         const otherDID = "did:key:EXAMPLE"
+        // const otherDID = await wn.did.ucan()
+        console.log('other', otherDID)
+
+
+
+
+        // https://webnative.fission.app/modules/ucan.html#sign
+
+
+
 
         /**
          * The UCAN, encoded as a string.
          */
         wn.ucan.build({
+            // audience should be a DID
+            // (audience is a publicKey)
             audience: otherDID,
             issuer: ourDID,
             lifetimeInSeconds: 60 * 60 * 24, // UCAN expires in 24 hours
+            potency: 'APPEND_ONLY',
             proof: possibleProof
         })
             .then((ucan) => {
-                console.log('got ucan', ucan)
+                console.log('**got ucan**', ucan)
+
+                wn.ucan.isValid(ucan)
+                    .then(val => console.log('**is valid**', val))
+
+                // var enc = wn.ucan.encode(ucan)
+                // console.log('enc', enc)
+
+                // try {
+                //     var dec = wn.ucan.decode(enc)
+                //     console.log('dec', dec)
+                // } catch (err) {
+                //     console.log('errrrrr', err)
+                // }
+
+                wn.ucan.sign(ucan.header, ucan.payload)
+                    .then((res) => {
+                        console.log('**sign**', res)
+                    })
             })
     })
