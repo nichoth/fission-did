17906f2617c935fadab0e20804805ed9de5b39d9
nichoth <nichoth@gmail.com>
2021-09-06 17:03:04 -0700

    use a ui

--- example.js
+++ example.js
@@ -1,9 +1,55 @@
+import { render } from 'preact'
+import { useState } from 'preact/hooks'
+import { html } from 'htm/preact'
+var xtend = require('xtend')
+
 const wn = window.webnative
 // import * as wn from 'webnative'
 
 // const UCANS_STORAGE_KEY = 'webnative.auth_ucans'
 
 console.log('wn', wn)
+
+console.log('**crypt**', crypto.subtle)
+var subtle = crypto.subtle
+
+function Demonstration () {
+    var [state, setState] = useState({ idOne: null, idTwo: null })
+
+    console.log('**state**', state)
+
+    if (!state.idOne) {
+        subtle.generateKey({
+            name: "RSASSA-PKCS1-v1_5",
+            modulusLength: 2048,
+            publicExponent: new Uint8Array([ 0x01, 0x00, 0x01 ]),
+            hash: "SHA-256"
+        }, false, ['sign', 'verify'])
+            .then(keyPair => {
+                setState(xtend(state, { idOne: keyPair }))
+            })
+    }
+
+    if (!state.idTwo) {
+        wn.did.ucan().then(did => setState(xtend(state, { idTwo: did })))
+    }
+
+
+    return html`<div class="the-ui">
+        <ul class="msgs1"></ul>
+        <ul class="msgs2"></ul>
+        <div class="id1">
+            <h2>id one</h2>
+            <pre>${JSON.stringify(state.id)}</pre>
+        </div>
+        <div class="id2">
+            <h2>id two</h2>
+        </div>
+    </div>`
+
+}
+
+render(html`<${Demonstration} />`, document.getElementById('content'))
 
 // this returns the same value because the DID is a global variable
 // wn.did.ucan()
@@ -13,20 +59,45 @@ console.log('wn', wn)
 //         console.log('pk again', pk)
 //     })
 
+
 // DIDs
 wn.did.ucan()
-    .then(ourDID => {
+    .then(async ourDID => {
         // the public key used in the DID is saved in storage. This means
         // that `wn.did.ucan()` will return the same DID no whenever you
         // call it. (The DID is a global value). This is true if you call
         // it in new windows or even 'incognito' windows too.
 
         // note that the related private key is not exposed in any way.
-        // It can't be stolen b/c it is not readable by the webCrypto API
-        console.log('our did', ourDID)
+        // It can't be stolen b/c it is not readable.
+        // console.log('our did', ourDID)
 
         var pk = wn.did.didToPublicKey(ourDID)
         console.log('pk', pk)
+
+
+
+        // var storeB = wn.keystore.create({ storeName: 'b' })
+        // // console.log('sb', storeB)
+        // const storeA = wn.keystore.create({ storeName: 'a' })
+
+        // await wn.keystore.set(storeB)
+        // const issB = await wn.did.ucan()
+        // console.log('b', issB)
+
+        // await wn.keystore.set(storeA)
+        // const issA = await wn.did.ucan()
+        // console.log('a', issA)
+
+        // const prf = await wn.ucan.build({
+        //     audience: issB,
+        //     issuer: issA
+        // })
+
+        // console.log('prf', prf)
+
+
+
 
         wn.keystore.create()
             .then(async ks => {
@@ -87,7 +158,7 @@ wn.did.ucan()
 
                 wn.ucan.sign(ucan.header, ucan.payload)
                     .then((sig) => {
-                        console.log('**sign**', sig)
+                        console.log('**sign**', !!sig)
                     })
             })
     })
